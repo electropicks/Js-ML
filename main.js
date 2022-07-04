9a2682e8cd261ac6f74ec71debbd57a9bbd1d20a
electropicks <lkbidy@calpoly.edu>
2022-07-04 16:26:43 -0700

    keeping up with video

--- main.js
+++ main.js
@@ -7,18 +7,31 @@ const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width/2, carCanvas.width * 0.90);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
+
+const N = 100;
+const cars = generateCars(N);
+
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2.1)
 ];
 
 animate();
 
-function animate() {
+function generateCars(N) {
+    const cars = [];
+    for (let i = 1; i <= N; i++) {
+        cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI"));
+    }
+    return cars;
+}
+
+function animate(time) {
     for (let i = 0; i < traffic.length; i++) {
         traffic[i].update(road.borders, []);
     }
-    car.update(road.borders, traffic);
+    for (let i = 0; i < cars.length; i++) {
+        cars[i].update(road.borders, traffic);
+    }
 
     carCanvas.height = window.innerHeight;
     networkCanvas.height = window.innerHeight;
@@ -34,6 +47,7 @@ function animate() {
 
     carCtx.restore();
 
+    networkCtx.lineDashOffset = -time/50;
     Visualizer.drawNetwork(networkCtx, car.brain);
     requestAnimationFrame(animate);
 }   
